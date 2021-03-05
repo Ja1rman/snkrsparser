6d43407c6fa58f7694a02e17b93b163d38363f69
Ja1rman <49319500+Ja1rman@users.noreply.github.com>
2021-03-05 13:38:54 +0300

    Update nike.py

--- nike.py
+++ nike.py
@@ -1,36 +1,65 @@
 import requests
-import bs4
+from bs4 import BeautifulSoup
+import traceback
+import discord_webhook
 
-def stock(url):
-    response = requests.get(url).text
-    styleCode = response[response.find('styleColor" content="')+21:]
-    styleCode = styleCode[:styleCode.find('"')]
+wb = 'https://discord.com/api/webhooks/817344618080501760/m3TA7dndamTgHYAL2YpW42izfAOhxMTrzmqrDvB9QSZg6mQiXC6T7cCkAdQXltQroXHu'
 
-    productId = response[response.find('productId" content="')+20:]
-    productId = productId[:productId.find('"')]
-    atk = url + '/?productId=' + productId + '&size='
+def nike():
+    all_items = []
 
-    url = 'https://api.nike.com/merch/skus/v2/?filter=productId%28' + productId + '%29&filter=country%28RU%29'
-    response = requests.get(url).json()['objects']
-    sizes = []
-    for size in response:
-        sizes.append(size['nikeSize'])
+    #check stock and atk
+    def stock(url):
+        response = requests.get(url, headers={
+                            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"}).text
+        styleCode = response[response.find('styleColor" content="')+21:]
+        styleCode = styleCode[:styleCode.find('"')]
 
-    url = 'https://api.nike.com/deliver/available_gtins/v2/?filter=styleColor%28' + styleCode + '%29&filter=merchGroup%28XP%29'
-    response = requests.get(url).json()['objects']
-    stocks = []
-    for stock in response:
-        stocks.append(stock['level'])
+        productId = response[response.find('productId" content="')+20:]
+        productId = productId[:productId.find('"')]
+        atk = url + '/?productId=' + productId + '&size='
 
-    stockSize = {}
-    for i in range(0, len(sizes)):
-        stockSize[sizes[i]] = stocks[i]
+        url = 'https://api.nike.com/merch/skus/v2/?filter=productId%28' + productId + '%29&filter=country%28RU%29'
+        response = requests.get(url, headers={
+                            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"}).json()['objects']
+        sizes = []
+        for size in response:
+            sizes.append(size['nikeSize'])
 
-    res = ''
-    for key, value in stockSize.items():
-        res += key + ' - ' + value + ' ' + atk + key + '\n'
-    return res
+        url = 'https://api.nike.com/deliver/available_gtins/v2/?filter=styleColor%28' + styleCode + '%29&filter=merchGroup%28XP%29'
+        response = requests.get(url, headers={
+                            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"}).json()['objects']
+        stocks = []
+        for stock in response:
+            stocks.append(stock['level'])
+
+        stockSize = {}
+        for i in range(0, len(sizes)):
+            stockSize[sizes[i]] = stocks[i]
+
+        res = ''
+        for key, value in stockSize.items():
+            res += key + ' - ' + value + ' ' + atk + key + '\n'
+        return res
+    
+    #parse new items
+    while True:
+        try:
+            response = requests.get('https://www.nike.com/ru/launch?s=upcoming', headers={
+                            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"}).text
+            soup = BeautifulSoup(response, 'html.parser')
+            products = soup.find_all('figure', {'class': 'pb2-sm va-sm-t ncss-col-sm-12 ncss-col-md-6 ncss-col-lg-4 pb4-md prl0-sm prl2-md ncss-col-sm-6 ncss-col-lg-3 pb4-md prl2-md pl1-md pr0-md d-sm-h d-md-ib'})
+            for product in products:
+                if product.get_text() in all_items: continue
+                all_items.append(product.get_text())
+                ptext = product.find('h3', {'class': 'headline-5'}).get_text()
+                ptext += product.find('h6', {'class': 'headline-3'}).get_text()
+                ptext = ptext.replace('\n', ' ')
+                ptext += '\n' + product.find('p', {'class': 'headline-4'}).get_text() + ' ' + product.find('p', {'class': 'headline-1'}).get_text() + '\n'
+                ptext += stock('https://www.nike.com' + product.find('a', {'class': 'card-link d-sm-b'})['href'])
+                discord_webhook.DiscordWebhook(url=wb,
+                                            content=ptext).execute()
+        except: print(traceback.format_exc())
 
 if __name__ == '__main__':
-    with open('temp.txt', mode='r', encoding='utf-8') as f:
-        print(f.read())
+    nike()
